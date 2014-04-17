ef79f62f897c08539687a54750434416107f6326
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-04-17 15:53:12 -0700

    add routes file and get controller action for env

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,26 @@
 require "rulers/version"
-require "rulers/array"
-require "rulers/string"
+require "rulers/routing"
 
 module Rulers
   class Application
   	def call(env)
-  	  `echo debug > debug.txt`;
+  	  klass, act = get_controller_and_action(env)
+  	  controller = klass.new(env)
+  	  text = controller.send(act)
   	  [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers".exclamify(5)]]
+        [text]]
   	end
+  end
+
+  class Controller
+
+  	def initialize(env)
+  	  @env = env
+  	end
+
+  	def env
+  	  @env
+  	end
+
   end
 end
--- lib/rulers/string.rb
+++
@@ -1,5 +0,0 @@
-class String
-  def exclamify(intensity=1)
-  	self + ("!" * intensity)
-  end
-end
