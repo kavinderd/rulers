f164cfb385617de23a86280a288eff196943f6b6
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-18 07:48:36 -0700

    add update filemodel instance method

---
+++ lib/rulers/file_model.rb
@@ -0,0 +1,81 @@
+require "multi_json"
+
+module Rulers
+  module Model
+  	class FileModel
+
+  	  def initialize(filename)
+  	  	@filename = filename
+
+  	  	#If filename is dir/37.json, @id is 37
+  	  	basename = File.split(filename)[-1]
+  	  	@id = File.basename(basename, ".json").to_i
+  	  	obj = File.read(filename)
+  	  	@hash = MultiJson.load(obj)
+  	  	@hash
+  	  end
+
+  	  def [](name)
+  	  	@hash[name.to_s]
+  	  end
+
+  	  def []=(name, value)
+  	  	@hash[name.to_s] = value
+  	  end
+
+  	  def self.find(id)
+  	  	begin
+  	  	  FileModel.new("db/quotes/#{id}.json")
+  	  	rescue
+  	  	  return nil
+  	  	end
+  	  end
+
+  	  def self.all
+  	  	files = Dir["db/quotes/*.json"]
+  	  	files.map {|f| FileModel.new f }
+  	  end
+
+      def self.create(attrs)
+        hash = {}
+        hash["submitter"] = attrs["submitter"] || ""
+        hash["quote"] = attrs["quote"] || ""
+        hash["attribution"] = attrs["attribution"] || ""
+        files = Dir["db/quotes/*.json"]
+        names = files.map{|f| f.split("/")[-1]}
+        highest = names.map{|b| b.to_i}.max
+        puts "#{highest}"
+        id = highest + 1
+        File.open("db/quotes/#{id}.json", "w") do |f|
+          f.write <<TEMPLATE
+{
+  "submitter": "#{hash["submitter"]}",
+  "quote": "#{hash["quote"]}",
+  "attribution": "#{hash["attribution"]}"
+}
+TEMPLATE
+        end
+        FileModel.new "db/quotes/#{id}.json"
+      end
+
+      def update(params)
+        puts "hello?"
+        puts @hash
+        puts params
+        @hash =@hash.merge(params)
+        data = MultiJson.dump(@hash).to_s
+        puts data
+        begin
+          File.open(@filename, "r+") do |f|
+            f.write(data)
+          end
+        rescue Exception => e
+          puts e
+        end
+        puts "hello"
+        self
+      end
+
+  	end
+  end
+end
