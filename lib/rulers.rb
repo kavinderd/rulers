33647ecfaaffcc5ba18ddf48d48efd8e2a561758
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-25 20:06:05 -0700

    completed ch 9

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,28 +5,18 @@ require "rulers/dependencies"
 require "rulers/controller"
 require "rulers/file_model"
 require "rulers/view"
-
-
+require "pry"
+require "pry-debugger"
 module Rulers
   class Application
 
-  	def call(env)
-  	  if env['PATH_INFO'] == '/favicon.ico'
-  	  	return [404, 
-  	  		{'Content-Type' => 'text/html'}, []]
+    def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
       end
-  	  klass, act = get_controller_and_action(env)
-  	  controller = klass.new(env)
-      text = controller.send(act)
-      if controller.get_response
-        st, hd, rs = controller.get_response.to_a
-        [st, hd, [rs.body].flatten]
-      else
-        
-  	    [200, {'Content-Type' => 'text/html'},
-          controller.render(act)]
-      end
-  	end
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
+    end
   end
   
 end
